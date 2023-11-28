01fadb8be6524e983faa1e0025f92afa473a5ad2
SRIRAM YERRA <132144497+sriram-yerra@users.noreply.github.com>
2023-11-28 22:09:53 +0530

    Add files via upload

---
+++ SortAnArrayOf 0s, 1s and 2s.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def sortColors(self, nums):
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+        return nums
+
+nums = [1,0,2,0,1]
+sol = Solution()
+res = sol.sortColors(nums)
+print(res)
+
